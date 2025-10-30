054788dc0f6b881f25cd5ff1d79187a9a759470f
JeanMSalazar <113962946+JeanMSalazar@users.noreply.github.com>
2025-10-30 17:25:52 -0500

    primer

--- src/federated_main.py
+++ src/federated_main.py
@@ -29,9 +29,13 @@ if __name__ == '__main__':
     args = args_parser()
     exp_details(args)
 
-    if args.gpu_id:
-        torch.cuda.set_device(args.gpu_id)
-    device = 'cuda' if args.gpu else 'cpu'
+    
+    if args.gpu is not None and torch.cuda.is_available():
+        torch.cuda.set_device(int(args.gpu))
+        device = 'cuda'
+    else:
+        device = 'cpu'
+
 
     # load dataset and user groups
     train_dataset, test_dataset, user_groups = get_dataset(args)
@@ -125,7 +129,7 @@ if __name__ == '__main__':
     file_name = '../save/objects/{}_{}_{}_C[{}]_iid[{}]_E[{}]_B[{}].pkl'.\
         format(args.dataset, args.model, args.epochs, args.frac, args.iid,
                args.local_ep, args.local_bs)
-
+    os.makedirs(os.path.dirname(file_name), exist_ok=True)
     with open(file_name, 'wb') as f:
         pickle.dump([train_loss, train_accuracy], f)
 
